5d2e3ad37f1462e4755f511341d22a9e0478c76b
alnutile <alfrednutile@gmail.com>
2014-08-07 09:34:35 -0400

    Repsonse code

--- src/App/Services/ResponseServices.php
+++ src/App/Services/ResponseServices.php
@@ -11,4 +11,8 @@ namespace App\Services;
 
 class ResponseServices implements ResponseInterface {
 
+    public static function respond($data, $message)
+    {
+        return ['data' => $data, 'message' => $message];
+    }
 } 
